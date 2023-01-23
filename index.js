cb1523fe28c0b55c8f74219f631fcf7bb45a927a
liviodimola <109294880+liviodimola@users.noreply.github.com>
2023-01-23 19:38:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-let display = document.getElementById("display");
-let decrement = document.getElementById("decrement");
-let increment = document.getElementById("increment");
-let reset = document.getElementById("reset");
+const div = document.createElement("div");
+div.id = "container";
+document.body.appendChild(div);
+
+const display = document.createElement("div");
+display.id = "display";
+div.appendChild(display);
+
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "decrement_increment_container";
+div.appendChild(buttonContainer);
+
+const decrement = document.createElement("button");
+decrement.id = "decrement";
+buttonContainer.appendChild(decrement);
+
+const increment = document.createElement("button");
+increment.id = "increment";
+buttonContainer.appendChild(increment);
+
+const reset = document.createElement("button");
+reset.id = "reset";
+div.appendChild(reset);
+
 let displayState = 0;
 
 display.innerHTML = 0;
